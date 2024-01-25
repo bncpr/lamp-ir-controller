adf32da55d440dbb87f80f4d4378c9c068ec2eff
Ben Cooper <benc3804@gmail.com>
2024-01-25 16:39:17 +0200

    init project

---
+++ ir_recv.ino
@@ -0,0 +1,17 @@
+#include <IRremote.hpp>
+
+#define IR_RECEIVE_PIN 8
+
+void setup() {
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);  // Start the receiver
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (IrReceiver.decode()) {
+      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); // Print "old" raw data
+      IrReceiver.printIRResultShort(&Serial); // Print complete received data in one line
+      IrReceiver.printIRSendUsage(&Serial);   // Print the statement required to send this data
+      IrReceiver.resume(); // Enable receiving of the next value
+  }
+}
